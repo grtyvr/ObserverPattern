b1000a0f0e88b51d21d31d2fbcade2bb0e5958ee
Guy Thomas <guy@grtyvr.com>
2022-01-10 17:46:01 -0800

    code cleanup to the push model implementaton

--- src/main.cpp
+++ src/main.cpp
@@ -40,12 +40,12 @@ public:
 
 	void registerObserver(Observer *observer) override
 	{
-		// std::cout << "Register new observer" << std::endl;
+		std::cout << "Register new observer" << std::endl;
 		observers_.push_back(observer);
 	}
 	void removeObserver(Observer *observer) override
 	{
-		std::cout << "Signing out...." << std::endl;
+		std::cout << "Observer removed from the list." << std::endl;
 		observers_.remove(observer);
 	}
 	void notifyObserver() override
@@ -77,8 +77,7 @@ public:
 
 	void HowManyObservers()
 	{
-		std::cout << "there are " << observers_.size() << " observers currently.\n"
-				  << std::endl;
+		std::cout << "there are " << observers_.size() << " observers currently." << std::endl;
 	}
 };
 
@@ -96,12 +95,12 @@ public:
 	GenericObserver(WeatherStation &weatherStation) : weatherStation_(weatherStation)
 	{
 		this->weatherStation_.registerObserver(this);
-		std::cout << "Generic subscriber ..." << ++GenericObserver::static_number_ << std::endl;
+		std::cout << "Generic Display subscribing..." << ++GenericObserver::static_number_ << std::endl;
 		this->number_ = GenericObserver::static_number_;
 	}
 	virtual ~GenericObserver()
 	{
-		std::cout << "Unsubscribe! was subscriber " << std::endl;
+		std::cout << "Goodby" << std::endl;
 	}
 	void Update(float temperature, float humidity, float pressure) override
 	{
@@ -109,207 +108,226 @@ public:
 		this->humidity = humidity;
 		display();
 	}
-	void RemoveMeFromTheList() {
-    weatherStation_.removeObserver(this);
-    std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
-  	}
+	void RemoveMeFromTheList()
+	{
+		weatherStation_.removeObserver(this);
+	}
 	void display() override
 	{ // display current measurements
 		std::cout << "Generic Display:" << temperature << " F" << std::endl;
 	}
 };
 
+class CurrentConditionsDisplay : public Observer, public DisplayElement
+{
+private:
+	float temperature;
+	float humidity;
+	WeatherStation &weatherStation_;
 
-class CurrentConditionsDisplay : public Observer, public DisplayElement {
-	private:
-		float temperature;
-		float humidity;
-		float pressure;
-		WeatherStation &weatherStation_;
-		static int static_number_;
-  		int number_;
-
-	public:
-	CurrentConditionsDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation){
+public:
+	CurrentConditionsDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation)
+	{
 		this->weatherStation_.registerObserver(this);
-		std::cout << "Current Conditions subscriber ..." << ++CurrentConditionsDisplay::static_number_ << std::endl;
-//		this->number_ = CurrentConditionsDisplay::static_number_;
+		std::cout << "Current Conditions subscribing..." << std::endl;
 	}
-	virtual ~CurrentConditionsDisplay() {
-		std::cout << "Unsubscribe!" << std::endl;
+	virtual ~CurrentConditionsDisplay()
+	{
+		std::cout << "Goodby." << std::endl;
 	}
-	void Update(float temperature, float humidity, float pressure) override {
+	void Update(float temperature, float humidity, float pressure) override
+	{
 		this->temperature = temperature;
 		this->humidity = humidity;
 		display();
 	}
-	void display() override { // display current measurements
+	void display() override
+	{ // display current measurements
 		std::cout << "Current Conditions:" << temperature << " F degress and "
-						<< humidity << " humidity" << std::endl;
+				  << humidity << " humidity" << std::endl;
 	}
-	void RemoveMeFromTheList() {
-    weatherStation_.removeObserver(this);
-    std::cout << "Observer removed from the list.\n";
-    // std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
-  	}
+	void RemoveMeFromTheList()
+	{
+		weatherStation_.removeObserver(this);
+	}
 };
 
-class StatisticsDisplay : public Observer, public DisplayElement {
-	private:
-		float maxTemp = 0.0;
-		float minTemp = 200;
-		float tempSum = 0.0;
-		int numReadings;
-		WeatherStation &weatherStation_;
-	public:
-	StatisticsDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation) {
+class StatisticsDisplay : public Observer, public DisplayElement
+{
+private:
+	float maxTemp = 0.0;
+	float minTemp = 200;
+	float tempSum = 0.0;
+	int numReadings;
+	WeatherStation &weatherStation_;
+
+public:
+	StatisticsDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation)
+	{
 		std::cout << "Statistics display subscribing..." << std::endl;
 		this->weatherStation_.registerObserver(this);
 	}
-	void Update(float temperature, float humidity, float presure) override {
+	virtual ~StatisticsDisplay()
+	{
+		std::cout << "Goodby" << std::endl;
+	}
+	void Update(float temperature, float humidity, float presure) override
+	{
 		tempSum += temperature;
 		numReadings += 1;
-		if (temperature > maxTemp){
+		if (temperature > maxTemp)
+		{
 			maxTemp = temperature;
 		}
-		if (temperature < minTemp){
+		if (temperature < minTemp)
+		{
 			minTemp = temperature;
 		}
 		display();
 	}
-	void display() override { // display ave, min, max
-		std::cout << "Average/Max/Min temperature:" << tempSum/numReadings << "/"
-						<< maxTemp << "/" << minTemp << std::endl;
-
+	void display() override
+	{ // display ave, min, max
+		std::cout << "Average/Max/Min temperature:" << tempSum / numReadings << "/"
+				  << maxTemp << "/" << minTemp << std::endl;
+	}
+	void RemoveMeFromTheList()
+	{
+		weatherStation_.removeObserver(this);
 	}
 };
 
-class ForecastDisplay : public Observer, public DisplayElement {
-	private:
-		float currentPressure = 29.92;
-		float lastPressure;
-		WeatherStation &weatherStation_;
-	public:
-	ForecastDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation){
+class ForecastDisplay : public Observer, public DisplayElement
+{
+private:
+	float currentPressure = 29.92;
+	float lastPressure;
+	WeatherStation &weatherStation_;
+
+public:
+	ForecastDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation)
+	{
 		std::cout << "Forecast display subscribing..." << std::endl;
 		this->weatherStation_.registerObserver(this);
 	}
-	void Update(float temperature, float humidity, float pressure) override {
+	virtual ~ForecastDisplay()
+	{
+		std::cout << "Goodby" << std::endl;
+	}
+	void Update(float temperature, float humidity, float pressure) override
+	{
 		lastPressure = currentPressure;
 		currentPressure = pressure;
 		display();
 	}
-	void display() override { // display forecast
+	void display() override
+	{ // display forecast
 		std::cout << "Forecast:";
-		if (currentPressure > lastPressure) {
+		if (currentPressure > lastPressure)
+		{
 			std::cout << "Pressure rising --> good weather on the way.";
-		} else if (currentPressure = lastPressure) {
+		}
+		else if (currentPressure = lastPressure)
+		{
 			std::cout << "no changes forecast.";
-		} else if (currentPressure < lastPressure) {
+		}
+		else if (currentPressure < lastPressure)
+		{
 			std::cout << "Pressure falling --> a change is gonna come!";
 		}
 		std::cout << std::endl;
 	}
+	void RemoveMeFromTheList()
+	{
+		weatherStation_.removeObserver(this);
+	}
 };
 
-class HeatindexDisplay : public Observer, public DisplayElement {
-	private:
-		float temperature_;
-		float humidity_;
-		WeatherStation &weatherStation_;
-	public:
-	HeatindexDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation){
+class HeatindexDisplay : public Observer, public DisplayElement
+{
+private:
+	float temperature_;
+	float humidity_;
+	WeatherStation &weatherStation_;
+
+public:
+	HeatindexDisplay(WeatherStation &weatherStation) : weatherStation_(weatherStation)
+	{
 		std::cout << "Heatindex display subscribing..." << std::endl;
 		this->weatherStation_.registerObserver(this);
 	}
-	void Update(float temperature, float humidity, float pressure) override {
+	virtual ~HeatindexDisplay()
+	{
+		std::cout << "Goodby" << std::endl;
+	}
+	void Update(float temperature, float humidity, float pressure) override
+	{
 		temperature_ = temperature;
 		humidity_ = humidity;
 		display();
 	}
-	void display() override { // display forecast
+	void display() override
+	{ // display forecast
 		std::cout << "Heat Index: is " << Heatindex(temperature_, humidity_) << std::endl;
 	}
-	float Heatindex(float t, float rh){
-		float index = (float)((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) 
-			+ (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) 
-			+ (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
-			(0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 * 
-			(rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) + 
-			(0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
-			0.000000000843296 * (t * t * rh * rh * rh)) -
-			(0.0000000000481975 * (t * t * t * rh * rh * rh)));
+	float Heatindex(float t, float rh)
+	{
+		float index = (float)((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) + (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) + (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+							   (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 * (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) +
+							   (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+							   0.000000000843296 * (t * t * rh * rh * rh)) -
+							  (0.0000000000481975 * (t * t * t * rh * rh * rh)));
 		return index;
+	}
+	void RemoveMeFromTheList()
+	{
+		weatherStation_.removeObserver(this);
 	}
 };
 
 int GenericObserver::static_number_ = 0;
-int CurrentConditionsDisplay::static_number_ = 0;
 
-void ClientCode()
+int main(int argc, char *argv[])
 {
 	// start up our weather station
 	WeatherStation *weatherStation = new WeatherStation;
 	// register some observers
-	// GenericObserver *observer1 = new GenericObserver(*weatherStation);
-	// GenericObserver *observer2 = new GenericObserver(*weatherStation);
-	// GenericObserver *observer3 = new GenericObserver(*weatherStation);
+	GenericObserver *observer1 = new GenericObserver(*weatherStation);
+	GenericObserver *observer2 = new GenericObserver(*weatherStation);
+	GenericObserver *observer3 = new GenericObserver(*weatherStation);
 	CurrentConditionsDisplay *currentConditions1 = new CurrentConditionsDisplay(*weatherStation);
 	HeatindexDisplay *heatIndexDeisplay = new HeatindexDisplay(*weatherStation);
-	// StatisticsDisplay *Statistics1 = new StatisticsDisplay(*weatherStation);
-	// GenericObserver *observer4;
-	// GenericObserver *observer5;
+	StatisticsDisplay *Statistics1 = new StatisticsDisplay(*weatherStation);
+	GenericObserver *observer4;
+	GenericObserver *observer5;
 
 	weatherStation->setMeasurements(80, 65, 30.4);
 
-	// observer3->RemoveMeFromTheList();
+	observer3->RemoveMeFromTheList();
 
 	weatherStation->setMeasurements(82, 70, 29.5);
 
-	//observer4 = new GenericObserver(*weatherStation);
+	observer4 = new GenericObserver(*weatherStation);
 
-	//observer2->RemoveMeFromTheList();
-	//observer5 = new GenericObserver(*weatherStation);
+	observer2->RemoveMeFromTheList();
+	observer5 = new GenericObserver(*weatherStation);
 
-	//weatherStation->setMeasurements(78, 90, 29.2);
+	weatherStation->setMeasurements(78, 90, 29.2);
 
-	//observer5->RemoveMeFromTheList();
+	observer5->RemoveMeFromTheList();
 
-	//weatherStation->setMeasurements(78, 90, 29.1);
-	//observer4->RemoveMeFromTheList();
-	//observer1->RemoveMeFromTheList();
-	//weatherStation->setMeasurements(78, 90, 29.2);
+	weatherStation->setMeasurements(78, 90, 29.1);
+	observer4->RemoveMeFromTheList();
+	observer1->RemoveMeFromTheList();
+	weatherStation->setMeasurements(78, 90, 29.2);
 	currentConditions1->RemoveMeFromTheList();
 
-	//delete observer5;
-	//delete observer4;
-	//delete observer3;
-	//delete observer2;
-	//delete observer1;
+	delete observer5;
+	delete observer4;
+	delete observer3;
+	delete observer2;
+	delete observer1;
 	delete currentConditions1;
 	delete weatherStation;
-}
-
-int main(int argc, char *argv[])
-{
-	ClientCode();
 	return 0;
-	// std::cout << "Starting Weather Station simulation" << std::endl;
-	// Subject *subject = new Subject();
-	// std::cout << "subcriptions coming..." << std::endl;
-	// Observer *observer1 = new Observer(*subject);
-	// std::cout << "second one..." << std::endl;
-	// CurrentConditionsDisplay *currentConditionsDisplay2 = new CurrentConditionsDisplay(*weatherData);
-	// //	StatisticsDisplay *statisticsDisplay = new StatisticsDisplay(*weatherData);
-	// //	ForecastDisplay *forecastDisplay = new ForecastDisplay(*weatherData);
 
-	// weatherData->setMeasurements(80, 65, 30.4);
-	// weatherData->setMeasurements(82, 70, 29.5);
-	// weatherData->setMeasurements(78, 90, 29.2);
-
-	// delete currentConditionsDisplay1;
-	// delete currentConditionsDisplay2;
-	// //	delete statisticsDisplay;
-	// //	delete forecastDisplay;
-	// delete weatherData;
 }
